690c94832ee158099f9ef7cf7cb3215dfd344b8a
lizarazo <d.lizarazo@utp.edu.co>
2025-09-17 10:49:23 -0500

    Update Flask server startup in main.py to use environment variable for port configuration and add eventlet monkey patching for improved WebSocket handling.

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,16 @@ This script will launch the Flask server which includes the WebSocket handling.
 import os
 import sys
 
+# Monkey patch for socketio server
+import eventlet
+eventlet.monkey_patch()
+
 from .scoring_server import app, socketio
 from .log_config import logger
 
 
 if __name__ == '__main__':
-    logger.info("Starting server on 127.0.0.1:5000")
-    socketio.run(app, host='127.0.0.1', port=5000, debug=False)
-    logger.info("Server started on 127.0.0.1:5000")
+    port = int(os.environ.get('PORT', 5000))
+    logger.info(f"Starting server on 127.0.0.1:{port}")
+    socketio.run(app, host='127.0.0.1', port=port, debug=False)
+    logger.info(f"Server started on 127.0.0.1:{port}")
